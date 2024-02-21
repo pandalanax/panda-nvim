62ce4289eb1a4253cd98c1f9062c7356ef752709
pandalanax <oskar.g@posteo.net>
2024-02-21 15:22:10 +0100

    added some useful remaps

--- config/lua/remap.lua
+++ config/lua/remap.lua
@@ -3,7 +3,13 @@ vim.g.mapleader = " "
 vim.opt.relativenumber = true
 vim.opt.nu = true
 
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex) -- project view
+vim.keymap.set("n", "n", "nzzzv") -- keeps cursos in middle when searching
+vim.keymap.set("n", "N", "Nzzzv")
+vim.keymap.set({"n", "v"}, "<leader>y", [["+y]]) -- copy to system clipboard
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]]) -- replace current word with
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
