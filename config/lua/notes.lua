ccc4c7fe2da5bda62a00151130dca3c8a3474c00
pandalanax <oskar.g@posteo.net>
2024-03-20 07:07:35 +0100

    dynamic path for notes depending on system

--- config/lua/notes.lua
+++ config/lua/notes.lua
@@ -1,4 +1,15 @@
-require("notes").setup({
+-- if mac then different path for notes, else is always HOME + /archive/notes
 
-	root = os.getenv("HOME") .. "/Documents/archive/notes/",
+local output = vim.fn.system({ "uname" })
+
+local output_uname = string.gsub(output, "\n", "")
+local notes_path = os.getenv("HOME") .. "/archive/notes"
+
+if output_uname == "Darwin" then
+	notes_path = os.getenv("HOME") .. "/Documents/archive/notes/"
+end
+
+print(notes_path)
+require("notes").setup({
+	root = notes_path,
 })
