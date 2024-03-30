a0d5f5d28320310acd940d692c55a28c687459b4
pandalanax <oskar.g@posteo.net>
2024-03-30 09:32:15 +0100

    buffer and cmdline history

--- config/luanix/nvim-lspconfig.nix.lua
+++ config/luanix/nvim-lspconfig.nix.lua
@@ -251,8 +251,28 @@ cmp.setup {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
     { name = 'path' },
+    { name = 'buffer', keyword_length = 4 },
   },
+
+  experimental = {
+    ghost_text= true,
+    native_menu = false,
+  }
 }
+-- `:` cmdline setup.
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline(),
+  sources = cmp.config.sources({
+    { name = 'path' }
+  }, {
+    {
+      name = 'cmdline',
+      option = {
+        ignore_cmds = { 'Man', '!' }
+      }
+    }
+  })
+})
 
 
 ''
