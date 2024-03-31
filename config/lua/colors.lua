aa7ae76eabda8c973effc7e7a0fb1b11561254d4
pandalanax <oskar.g@posteo.net>
2024-03-31 08:04:07 +0200

    hard everforest

--- config/lua/colors.lua
+++ config/lua/colors.lua
@@ -1,4 +1,10 @@
 -- require("colorbuddy").colorscheme("gruvbuddy")
 -- vim.cmd.colorscheme("onedark")
 --vim.cmd.colorscheme("gruvbuddy")
+
+require("everforest").setup({
+	---Controls the "hardness" of the background. Options are "soft", "medium" or "hard".
+	---Default is "medium".
+	background = "hard",
+})
 vim.cmd([[colorscheme everforest]])
