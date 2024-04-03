b8135374af7e8c55206b93ad2f0c0dc9120a96f0
pandalanax <oskar.g@posteo.net>
2024-04-03 18:32:25 +0200

    fail

--- config/lua/remap.lua
+++ config/lua/remap.lua
@@ -21,8 +21,8 @@ vim.keymap.set("n", "<C-i>", "<C-u>zz")
 vim.keymap.set("n", "<leader>gc", function()
 	require("treesitter-context").go_to_context(vim.v.count1)
 end, { silent = true })
-vim.keymap.set("v", "e", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "i", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "E", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "I", ":m '<-2<CR>gv=gv")
 
 vim.opt.wrap = false
 require("leap").create_default_mappings()
