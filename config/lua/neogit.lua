d582d41f0d3d45f62777c2a14cb8c672bd270d2c
pandalanax <oskar.g@posteo.net>
2024-04-05 16:19:04 +0200

    open neogit in a pane

--- config/lua/neogit.lua
+++ config/lua/neogit.lua
@@ -1,7 +1,7 @@
 -- init.lua
 local neogit = require("neogit")
 neogit.setup({})
-vim.keymap.set("n", "<leader>gs", neogit.open, { silent = true, noremap = true })
+vim.keymap.set("n", "<leader>gs", ":Neogit kind=vsplit<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>gc", ":Neogit commit<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>gp", ":Neogit pull<CR>", { silent = true, noremap = true })
 vim.keymap.set("n", "<leader>gP", ":Neogit push<CR>", { silent = true, noremap = true })
